3448a641bc7a03b1976707c06ea83b2e8c8a9f96
J1nn0x <quentinlepage258@gmail.com>
2024-01-03 17:35:13 +0100

    nbMac

--- assets/js/calculette.js
+++ assets/js/calculette.js
@@ -30,6 +30,12 @@ window.addEventListener("DOMContentLoaded", function () {
   function nombreMac(a) {
     let n = a / 100;
     n = Math.floor(n);
+    localStorage.setItem(
+      "affichageMacaron",
+      JSON.stringify({
+        nbMacaron: n,
+      })
+    );
     return n;
   }
 
@@ -50,7 +56,7 @@ window.addEventListener("DOMContentLoaded", function () {
     );
 
     const url =
-      "http://qlepage.alwaysdata.net/passion-marathons-etud/pages/calculette.php?nbMac=" +
+      "http://localhost/passion-marathons-etud/pages/calculette.php?nbMac=" +
       encodeURIComponent(nbMac);
     window.location.href = url;
   });
@@ -62,7 +68,12 @@ window.addEventListener("DOMContentLoaded", function () {
   }
   const dernierCom = localStorage.getItem("dernierCom");
   if (dernierCom) {
-    const { message } = JSON.parse(dernierCom);
-    com.innerHTML = `${message} Voici l'équivalent brulé en macarons :`;
+    const { message, nbMacaron } = JSON.parse(dernierCom);
+    com.innerHTML = `${message}`;
+  }
+  const affichageMacaron = localStorage.getItem("affichageMacaron");
+  if (affichageMacaron) {
+    const { nbMacaron } = JSON.parse(affichageMacaron);
+    com.innerHTML = `Cela équivaut à ${nbMacaron} macarons !`;
   }
 });
